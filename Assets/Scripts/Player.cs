adb6282a490e8cef9b32f49546d8d239e3565483
Alper <alperkuru98@hotmail.com>
2021-10-18 14:02:25 +0300

    Commit 1
    Player, CamFollow, Rotator and Level Spawner Scripts added. Also assets added.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private bool smash;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            smash = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            smash = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            smash = true;
+            rb.velocity = new Vector3(0, -100 * Time.fixedDeltaTime * 7, 0);
+        }
+
+        if (rb.velocity.y > 5)
+            rb.velocity = new Vector3(rb.velocity.x, 5, rb.velocity.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!smash)
+        {
+            rb.velocity = new Vector3(0, 50 * Time.deltaTime * 5, 0);
+        }
+        else
+        {
+            if(collision.gameObject.tag == "enemy")
+            {
+                Destroy(collision.transform.parent.gameObject);
+            }
+
+            if(collision.gameObject.tag == "plane")
+            {
+                print("Game Over");
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!smash)
+        {
+            rb.velocity = new Vector3(0, 50 * Time.deltaTime * 5, 0);
+        }
+    }
+}
